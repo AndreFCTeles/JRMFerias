acd0eda6ced26a27e67d9e07f5e4e3fb7587b06f
Gestor <gestor@ELECTREX.local>
2025-07-02 17:27:24 +0100

    02/07/2025 - Alteração de vite.config.ts para refletir mudanças a API

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   /* dev */
+  base: '/ferias/', 
   server: {
     host: '0.0.0.0',
     proxy: {
